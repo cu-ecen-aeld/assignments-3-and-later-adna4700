dd716782957645ed3bc06e006a1a9df2854732e3
aditi <aditi@Ubuntu.myguest.virtualbox.org>
2023-02-04 11:05:36 -0700

    changes complete

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -8,15 +8,44 @@
  *   value was returned by the command issued in @param cmd.
 */
 bool do_system(const char *cmd)
-{
+{   
+    //setting up system log
+    openlog(NULL, 0, LOG_USER);
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
+    int return_from_system = system(cmd);
+    
+    if(cmd == NULL)
+	{
+    	if(return_from_system != 0)
+		{
+			syslog(LOG_ERR, "SHELL available");
+			closelog();
+			return false;
+    		}
+	if(return_from_system == 0)
+		{
+			syslog(LOG_ERR, "SHELL isn't available");
+			closelog();
+			return false;				
+    		}
+	}
+     if(return_from_system == -1)
+     	{
+		//Child process could not be created or its status could not be retrived
+		syslog( LOG_ERR, "Error: %s", strerror(errno));
+		closelog();
+		return false;    		
+     	}
+     if(return_from_system  == 127)
+	{
+		//Child process created but the child shell terminated
+		syslog( LOG_ERR, "Child shell terminated");	
+		closelog();
+		return false; 	
+	}
+    
+    //NO error occurs; system() returns success 
+    closelog();    
     return true;
 }
 
@@ -49,18 +78,71 @@ bool do_exec(int count, ...)
     // and may be removed
     command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    openlog();
+    pid_t return_from_fork = fork();
+    if(return_from_fork == -1) 
+    	{
+		//Child process could not be created 
+		syslog( LOG_ERR, "Error: %s", strerror(errno));
+		closelog();
+		return false;
+		     		
+    	} 
+    if(return_from_fork == 0)
+    	{
+		//Child process created 
+		syslog("Child process created");
+		//call execv() 	
+		int return_excev = execv(command[0],command);
+		//returns only on failure		
+		if(return_execv == -1)
+			{
+				syslog(LOG_ERR, "Error: %s", strerror(errno));
+				closelog();
+				return false;	
+			}
+    	}
+    //call wait() form parent
+    int w_status;
+    pid_t result_from_wait = waitpid(pid,&w_status, 0);
+    if(result_from_wait == -1)
+	{
+		//if error occurs 
+		syslog( LOG_ERR, "Error: %s", strerror(errno));
+		closelog();
+		return false;
+	}
+
+    //if no error check for status
+    if(WIFEXITED(w_status))
+	{
+		syslog( LOG_ERR, "Child terminated normally: %s", WEXITSTATUS(w_status));
+		if(WEXITSTATUS(w_status != 0))
+			{
+				return false;
+			}
+	}
+    else if(WIFSIGNALED(w_status))
+    	{
+    		syslog( LOG_ERR, "Child terminated by a signal: %s", WTERMSIG(w_status));
+		if(WCOREDUMP)
+		{
+			syslog( LOG_ERR, "Child produced a core dump");
+		}
+    	}
+    else if(WTFSTOPPED(w_status))
+    	{
+    		syslog( LOG_ERR, "The number of signal which caused the child to stop",  WSTOPSIG());
+    	}
+
+	if(WTFCONTINUED(w_status))
+		{
+			syslog( LOG_ERR, "Child continued");
+		}
 
     va_end(args);
-
+    
+    closelog();
     return true;
 }
 
@@ -84,14 +166,75 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
+    int fd = open("redirected.txt", O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) { perror("open"); abort(); }
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    pid_t return_from_fork = fork();
+
+    if(return_from_fork == -1) 
+    	{
+		//Child process could not be created 
+		error("fork"); abort();
+		return false;
+		     		
+    	} 
+    if(return_from_fork == 0)
+    	{
+		//Child process created 
+		if (dup2(fd, 1) < 0) { perror("dup2"); abort(); }
+		//call execv() 	
+		int return_excev = execv(command[0],command);
+		//returns only on failure		
+		if(return_execv == -1)
+			{
+				perror("execvp"); abort();
+				close(fd);
+				return false;	
+			}
+    	}
+    //call wait() form parent
+    int w_status;
+    pid_t result_from_wait = waitpid(pid,&w_status, 0);
+    if(result_from_wait == -1)
+	{
+		//if error occurs 
+		perror( "Error: %d", errno);
+		close(fd);
+		return false;
+	}
+
+    //if no error check for status
+    if(WIFEXITED(w_status))
+	{
+		perror("Child terminated normally: %s", WEXITSTATUS(w_status));
+		close(fd);
+		if(WEXITSTATUS(w_status != 0))
+			{
+				return false;
+			}
+	}
+    else if(WIFSIGNALED(w_status))
+    	{
+    		perror( "Child terminated by a signal: %s", WTERMSIG(w_status));
+		if(WCOREDUMP)
+		{
+			perror("Child produced a core dump");
+		}
+		close(fd);
+    	}
+    else if(WTFSTOPPED(w_status))
+    	{
+    		perror("The number of signal which caused the child to stop",  WSTOPSIG());
+		close(fd);
+    	}
+
+	if(WTFCONTINUED(w_status))
+		{
+			perror("Child continued");
+			close(fd);
+		}
+
+
 
     va_end(args);
 
