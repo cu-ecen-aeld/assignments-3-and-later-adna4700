93e5a449604226ef26aa505df6a0fb35f9b36e63
aditi <aditi@Ubuntu.myguest.virtualbox.org>
2023-02-05 10:52:29 -0700

    All test passed

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -133,7 +133,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     pid_t return_from_fork, result_from_wait;
     int w_status;
     
-    int fd = open("redirected.txt", O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
     if(fd == -1)
     {
     	perror("Error creating a file");
