fa31f659e487a0a2d19c109904aca7fd494bf34f
Aditi Nanaware <adna4700@colorado.edu>
2023-03-09 23:56:16 -0700

    test case failure

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -14,6 +14,8 @@
 #else
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
 #endif
 
 #include "aesd-circular-buffer.h"
@@ -45,18 +47,59 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     }
 
     size_t find = char_offset;
+    printf("The char offset to find is %ld\r\n",find);
+
+    uint8_t length = buffer->out_offs - buffer->in_offs;
+    
+    if(length == 0)
+        if(buffer->full == true)
+            length = AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1;
+    
+    printf("The length is %d \r\n",length);
+
+    //offset shouldn't be greater than total length
+    int tracker =0;
+    size_t total_size;
+    while(tracker < length)
+    {
+        total_size += buffer->entry[tracker++].size;
+        tracker++;
+    }
+
+    printf("The total size is %ld\r\n", total_size);
+    if(find>total_size)
+        return NULL;
+
     size_t trace = 0;
-    int i=0;
-    do
+    int i= buffer->out_offs;
+    int track_len = 0;
+
+    printf("\r\nbefore while\r\n");
+    while((track_len <= (length)) && (trace<find) )
     {
         trace += buffer->entry[i].size;
-        i++;
+        printf("The trace is %ld \r\n", trace);
+        if(i == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1))
+            i = 0;
+        else 
+            i++;
 
-    }while((i<AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) && (trace<find));
+        printf("The new i is %d\r\n", i); 
+        track_len++;
+
+    };
+    printf("\r\nafter while\r\n");
+    // Print buffer entry 
+    printf("The buffer entry here is %s",buffer->entry[i].buffptr);
     //what we want is trace - find
      *entry_offset_byte_rtn = trace - find;
-     return &buffer->entry[i];    
+     return &(buffer->entry[i]);  
+   // return &(buffer->entry[i].buffptr(buffer->entry[i].size - (trace-find)));     
 }
+
+
+//out+offs  
+//in+offs
 
 /**
 * Adds entry @param add_entry to @param buffer in the location specified in buffer->in_offs.
@@ -77,29 +120,43 @@ int aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const st
     //else we can add doesn't matter if full or not 
    
     buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
-    buffer->entry[buffer->in_offs].size = add_entry->size;   
+    printf("Entry is %s\r\n", add_entry->buffptr);
+
+    printf("AAdded Entry is %s\r\n",  buffer->entry[buffer->in_offs].buffptr);
+    buffer->entry[buffer->in_offs].size = add_entry->size; 
+    printf("Added at offset %d\r\n", buffer->in_offs);  
 
     if(buffer->full == false)
     {
-        if(buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+        if(buffer->in_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1))
             buffer->in_offs  = 0;
         else
             buffer->in_offs += 1;
+
+        printf("New in_offs is %d\r\n", buffer->in_offs);
+        printf("New out_offs is %d\r\n", buffer->out_offs);
+
+        if(buffer->in_offs == buffer->out_offs)
+            buffer->full = true;
 
         return 0;
     }
     else
     {
         //buffer is full
-        if(buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+        if(buffer->in_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1))
             buffer->in_offs = 0;
         else
             buffer->in_offs += 1;
         
-        if(buffer->out_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+        printf("New in_offs is %d\r\n", buffer->in_offs);
+
+        if(buffer->out_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1))
             buffer->out_offs = 0;
         else
             buffer->out_offs += 1;
+
+        printf("New out_offs is %d\r\n", buffer->out_offs);
        
         return 0;
     }
