07ce3311c94c0c819468c4047a8ca031dd5dc9ed
Aditi Nanaware <adna4700@colorado.edu>
2023-03-18 11:22:18 -0600

    changes to resolve kernel error

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -181,13 +181,15 @@ const char* aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, 
 
     //else we can add doesn't matter if full or not 
    
+        buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+        buffer->entry[buffer->in_offs].size = add_entry->size; 
 
  
 
     if(buffer->full == false)
     {
-        buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
-        buffer->entry[buffer->in_offs].size = add_entry->size; 
+        //buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+        //buffer->entry[buffer->in_offs].size = add_entry->size; 
        
         if(buffer->in_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1))
             buffer->in_offs  = 0;
@@ -202,7 +204,7 @@ const char* aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, 
     else
     {
         //buffer is full
-        return_buffer =  buffer->entry[buffer->out_offs].buffptr;
+        return_buffer =  buffer->entry[buffer->in_offs].buffptr;
 
         if(buffer->out_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1))
             buffer->out_offs = 0;
@@ -211,8 +213,8 @@ const char* aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, 
 
        // printf("New out_offs is %d\r\n", buffer->out_offs);
 
-        buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
-        buffer->entry[buffer->in_offs].size = add_entry->size; 
+        //buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+        //buffer->entry[buffer->in_offs].size = add_entry->size; 
 
         if(buffer->in_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1))
             buffer->in_offs = 0;
