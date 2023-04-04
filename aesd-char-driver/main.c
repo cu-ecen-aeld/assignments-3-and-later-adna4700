d56a656a31b977fd6d3375e03f8399a9a1808709
Aditi Nanaware <adna4700@colorado.edu>
2023-04-04 10:20:51 -0600

    A9 changes

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -11,6 +11,21 @@
  *
  */
 
+/*
+Add custom seek support to your driver using the llseek file_operations function.  Your seek implementation should:
+
+Support all positional types (SEEK_SET, SEEK_CUR, and SEEK_END) and update the file position accordingly.
+
+For instance, if your driver is storing writes of 5, 7, and 9 bytes each, an file position of 0-5 would represent a byte within the first write, a file position of 6-13 would represent a byte within the second write, and a file position  of 14 through 23 would a byte within represent the 3rd write.
+
+For instance: Consider the content in the table below as handled by your driver, where each string ends with a newline character.  An offset of 15 would set the pointer to the second byte of the word “Singapore”, byte "i"
+
+			Grass
+
+			Sentosa
+
+			Singapore
+*/
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/printk.h>
@@ -19,6 +34,9 @@
 #include <linux/slab.h>
 #include <linux/fs.h> // file_operations
 #include "aesdchar.h"
+
+#include "aesd_ioctl.h"
+
 //#include "aesd-circular-buffer.h"
 #include <linux/string.h>
 #include <linux/uaccess.h>
@@ -31,6 +49,9 @@ MODULE_AUTHOR("Aditi Nanaware");
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
+
+
+
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
@@ -231,15 +252,186 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     exiting:
 	mutex_unlock(&device->mutex_lock);
 	return retval;
-
 }
+
+static long aesd_adjust_file_offset(struct file* filp, unsigned int write_cmd, unsigned int write_vmd_offset)
+{
+	struct aesd_dev *device = (struct aesd_dev*) filp->private_data;
+
+	loff_t offset;
+	int retvel;
+
+	if(filp == NULL)
+	{
+		PDEBUG("Error: Invalid file pointer ");
+		printk(KERN_DEBUG "Error: Invalid file pointer");
+
+		//Request not valid
+		retval = -EINVAL;
+		return retval;
+	}
+
+	if(mutex_lock_interruptible(&(device->mutex_lock)))
+    {
+        // If the process is interrupted, the function returns -ERESTARTSYS, indicating that the system call should be restarted
+		printk("Mutex Lock Failed \r\n");
+        PDEBUG(KERN_ERR "Mutex Lock Failed \r\n");
+		retval = -ERESTARTSYS;
+        return retval;
+	}
+
+	offset = get_offset(&device->circular_buff, write_cmd,write_vmd_offset);
+	//
+		printk("Offset Adjusting offset to %lld: The buffer number:  %d, Offset requested:  %d", offset, write_cmd, write_vmd_offset);
+        PDEBUG(KERN_ERR "Offset Adjusting offset to %lld: The buffer number:  %d, Offset requested:  %d", offset, write_cmd, write_vmd_offset);
+	//
+	
+	if(offset == -1)
+	{
+		//error
+		retval = -EINVAL;
+	}
+	else
+	{
+		//Setting/Adjusting the file postion to the offset 
+		filp->f_pos = offset;
+		//no error
+		retval = 0;
+	}
+
+	mutex_unlock(&(device->mutex_lock));
+
+	return retval;
+}
+
+#ifdef __KERNEL__
+loff_t get_offset(struct aesd_circular_buffer *buffer, unsigned int buffer_num, unsigned int offset_inside_buffer)
+{
+	int offset = 0, i = 0;
+
+	//check for invalid buffer number
+	if(buffer_num > (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) - 1)
+	{
+		printk("Invalid buffer number \r\n");
+    	PDEBUG(KERN_DEBUG "Invalid buffer number \r\n");		
+
+		return -1;
+	}
+
+	//check if the offset is within the range of the buffer size
+	if(offset_inside_buffer > (buffer->entry[buffer_num].size - 1))
+	{
+		printk("Offset not within the range of the buffer size \r\n");
+    	PDEBUG(KERN_DEBUG "Offset not within the range of the buffer size \r\n");		
+
+		return -1;
+	}
+
+	//Iterate through the buffer
+	for(i=0; i<buffer_num; i++)
+	{
+		if(buffer->entry[i].size == 0)
+		{
+			printk("Not enogh buffers loaded into the circular buffer list \r\n");
+    		PDEBUG(KERN_DEBUG "Not enogh buffers loaded into the circular buffer list \r\n");
+
+			return -1;
+		}
+		//increment the offset
+		offset = offset + buffer->entry[i].size;
+	}
+	//return the desired offset
+	return (offset + offset_inside_buffer);
+}
+
+#endif
+
 struct file_operations aesd_fops = {
 	.owner =    THIS_MODULE,
 	.read =     aesd_read,
 	.write =    aesd_write,
 	.open =     aesd_open,
 	.release =  aesd_release,
+	//
+	.llseek  = aesd_llseek,
+	.unlocked_ioctl = aesd_ioctl,
+	//
 };
+
+//llseek functionality
+loff_t aesd_llseek(struct file *filp, lofft_ offset, int whence)
+{
+	struct aesd_dev *device = (struct aesd_dev*) filp->private_data;
+	//return value
+	loff_t retval;
+
+	//checking for valid inputs
+
+	if(filp == NULL)
+	{
+		PDEBUG("Error: Invalid file pointer ");
+		printk(KERN_DEBUG "Error: Invalid file pointer");
+
+		//Request not valid
+		retval = -EINVAL;
+		return retval;
+	}
+
+	if(mutex_lock_interruptible(&(device->mutex_lock)))
+    {
+        // If the process is interrupted, the function returns -ERESTARTSYS, indicating that the system call should be restarted
+		printk(KERN_DEBUG "Mutex Lock Failed");
+        PDEBUG(KERN_ERR "Mutex Lock Failed");
+		retval = -ERESTARTSYS;
+        return retval;
+	}
+
+	retval = fixed_size_llseek(filp, offset, whence, aesd_device.new_string.size);
+
+	mutex_unlock(&device->mutex_lock);
+	return retval;
+
+}
+
+long aesd_ioctl(struct file * filp, unsigned int command, unsigned long args)
+{
+	long retval = 0;
+
+	struct aesd_seekto seekto;
+
+	//
+	if(_IOC_TYPE(command) != AESD_IOC_MAGIC)
+	{
+		//Request not valid
+		retval = -EINVAL;
+	}
+
+	if(_IOC_NR(command) > AESDCHAR_IOC_MAXNR)
+	{
+		//Request not valid
+		retval = -EINVAL;		
+	}
+
+	switch(command)
+	{
+		case AESDCHAR_IOCSEEKTO:
+		if(copy_from_user(&seekto, (const void __user*)args, sizeof(struct aesd_seekto))!= 0)
+		{
+			retval = -EFAULT;
+		}
+		else
+		{
+			retval = aesd_adjust_file_offset(filp, seekto.write_cmd, seekto.write_cmd_offset);
+		}
+		break;
+
+		default:
+			retval = -ENOTTY;
+			break;
+	}
+	return retval;
+}
+
 
 static int aesd_setup_cdev(struct aesd_dev *dev)
 {
